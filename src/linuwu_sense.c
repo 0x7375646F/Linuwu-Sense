98f65edf23b6232f8bd601024d21e765ebe723ca
Jake <jacobbrown@uchicago.edu>
2025-11-18 23:03:08 -0600

    Support for PHN16-73. RGB tested to work. All predator sense features tested to work.

--- src/linuwu_sense.c
+++ src/linuwu_sense.c
@@ -482,6 +482,11 @@ enum acer_wmi_predator_v4_oc {
     .predator_v4 = 1,
     .four_zone_kb = 1,
  };
+
+ static struct quirk_entry quirk_acer_predator_phn16_73 = {
+    .predator_v4 = 1,
+    .four_zone_kb = 1,
+ };
  
  static struct quirk_entry quirk_acer_nitro_an16_41 = {
     .nitro_v4 = 1,
@@ -772,7 +777,16 @@ enum acer_wmi_predator_v4_oc {
             DMI_MATCH(DMI_PRODUCT_NAME, "Predator PHN16-72"),
         },
         .driver_data = &quirk_acer_predator_phn16_72,
-    },
+     },
+     {
+        .callback = dmi_matched,
+        .ident = "Acer Predator PHN16-73",
+        .matches = {
+            DMI_MATCH(DMI_SYS_VENDOR, "Acer"),
+            DMI_MATCH(DMI_PRODUCT_NAME, "Predator PHN16-73"),
+        },
+        .driver_data = &quirk_acer_predator_phn16_73,
+     },
      {
          .callback = dmi_matched,
          .ident = "Acer Predator PH16-71",
